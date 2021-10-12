6469df88c7aeefaa05a5c10f8e43ca82d27f8665
Kreako <olivier@kreako.fr>
2021-10-12 15:46:38 +0200

    Actually build the files in the worker image

--- webhook/src/astro.py
+++ webhook/src/astro.py
@@ -1,3 +1,27 @@
+from datetime import datetime
+from sh import npm, git
+from path import Path
+
+
+KREAKO_CO_DIR = Path("/kreako.fr")
+ASTRO_DIR = KREAKO_CO_DIR / "astro"
+ASTRO_DIST_DIR = ASTRO_DIR / "dist"
+NGINX_DIR = "/nginx"
+
+
 def build(body):
-  from pprint import pprint
-  pprint(body)
+  # Remove The current dist folder
+  ASTRO_DIST_DIR.rmtree_p()
+
+  # Build
+  npm("run", "build", _cwd=ASTRO_DIR)
+
+  # Create the output dir
+  output = NGINX_DIR / datetime.now().isoformat()
+
+  # Copy from dist to output
+  ASTRO_DIST_DIR.copytree(output)
+
+  # Create the semaphore file to signal a complete build
+  with open(output / "done", "w") as f:
+    f.write("ok")
