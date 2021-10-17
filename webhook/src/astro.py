f8368f0593908674e9f3a7f2a9cdd6b28df9e44b
Kreako <olivier@kreako.fr>
2021-10-17 11:01:59 +0200

    generate data during build from worker

--- webhook/src/astro.py
+++ webhook/src/astro.py
@@ -1,12 +1,31 @@
 from datetime import datetime
 from sh import npm, git
 from path import Path
+import requests
+import json
 
 
 KREAKO_CO_DIR = Path("/kreako.fr")
+KREAKO_DATA = KREAKO_CO_DIR / "data"
+DATA_FNAME = KREAKO_DATA / "data.json"
 ASTRO_DIR = KREAKO_CO_DIR / "astro"
 ASTRO_DIST_DIR = ASTRO_DIR / "dist"
 NGINX_DIR = Path("/nginx")
+API_ENDPOINT = "http://127.0.0.1:1337"
+
+
+KREAKO_DATA.makedirs_p()
+
+
+def generate_data(fname):
+    def endpoint(e):
+        return f"{API_ENDPOINT}/{e}"
+
+    notes = requests.get(endpoint("notes")).json()
+    links = requests.get(endpoint("links")).json()
+    tags = requests.get(endpoint("tags")).json()
+    with open(fname, "w") as f:
+        json.dump(f, {"notes": notes, "links": links, "tags": tags})
 
 
 def build(body):
@@ -27,3 +46,10 @@ def build(body):
     # Create the semaphore file to signal a complete build
     with open(output / "done", "w") as f:
         f.write(now)
+
+    # Generate data
+    generate_data(KREAKO_DATA / "data.json")
+
+    # git commit and push
+    git("commit", "-m", "Update data", DATA_FNAME, _cwd=KREAKO_CO_DIR)
+    git("push")
