43f96d6f3d94c372ca0ef2d3278cef4f26c0cdfb
kreako <olivier@kreako.fr>
2022-02-28 14:36:40 +0100

    utility to convert description markdown -> html

--- next/lib/markdown.ts
+++ next/lib/markdown.ts
@@ -3,6 +3,17 @@ import remarkParse from "remark-parse"
 import remarkGfm from "remark-gfm"
 import remarkRehype from "remark-rehype"
 import rehypeStringify from "rehype-stringify"
+import { ContentType } from "./api"
+
+export const convertDescriptionInHtml = async (contents: ContentType[]) => {
+  for (const content of contents) {
+    if (content.kind === "link") {
+      content.link.description = await markdownToHtml(content.link.description)
+    } else {
+      content.note.description = await markdownToHtml(content.note.description)
+    }
+  }
+}
 
 export const markdownToHtml = async (source: string) => {
   const r = await unified()
