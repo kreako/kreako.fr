7921037bcc5ec1d0cbf99958b88277c39d0efc35
kreako <olivier@kreako.fr>
2022-04-16 15:48:38 +0200

    Allow page to select empty layout by adding a custom props and a custom type

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,13 +1,28 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import type { NextPage } from "next"
 import Layout from "../components/layout"
+import EmptyLayout from "../components/empty-layout"
+
+export type EmptyLayoutPage = NextPage & {
+  emptyLayout: boolean
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+  if ("emptyLayout" in Component) {
+    const Comp = Component as EmptyLayoutPage
+    return (
+      <EmptyLayout>
+        <Comp {...pageProps} />
+      </EmptyLayout>
+    )
+  } else {
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    )
+  }
 }
 
 export default MyApp
