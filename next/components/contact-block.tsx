550aaf62ca7f2e6da3fffefe1e8da83bddcca6ec
kreako <olivier@kreako.fr>
2022-04-18 07:06:41 +0200

    contact block : bigger text and no more bg gradient

--- next/components/contact-block.tsx
+++ next/components/contact-block.tsx
@@ -1,15 +1,15 @@
 export default function ContactBlock() {
   return (
-    <div className="flex flex-col items-center bg-gradient-to-br from-sky-500 to-sky-600 px-2 py-12">
+    <div className="flex flex-col items-center bg-sky-600 px-2 py-16 space-y-4">
       <div className="uppercase tracking-wider font-bold text-white text-sm">Contact</div>
-      <div className="grid grid-cols-1 gap-y-2 mt-6">
+      <div className="grid grid-cols-1 gap-y-4 mt-6">
         <div className="flex items-center space-x-2">
-          <div className="font-bold text-lg md:text-xl text-white">
+          <div className="font-bold text-xl md:text-2xl text-white">
             <a href="tel:+33698990069">+33 6 98 99 00 69</a>
           </div>
         </div>
         <div className="flex items-center space-x-2">
-          <div className="font-bold text-lg md:text-xl text-white">
+          <div className="font-bold text-xl md:text-2xl text-white">
             <a href="mailto:olivier@kreako.fr">olivier@kreako.fr</a>
           </div>
         </div>
