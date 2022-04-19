127631282ba4468bfc161cb6eb2d2795bafd92ef
kreako <olivier@kreako.fr>
2022-04-19 10:09:56 +0200

    icon arrow backward and forward

---
+++ next/icons/icon-arrow-backward.tsx
@@ -0,0 +1,7 @@
+export function IconArrowBackward() {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24">
+      <path d="M17.77 3.77L16 2L6 12l10 10l1.77-1.77L9.54 12z" fill="currentColor"></path>
+    </svg>
+  )
+}
---
+++ next/icons/icon-arrow-forward.tsx
@@ -0,0 +1,7 @@
+export function IconArrowForward() {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24">
+      <path d="M6.23 20.23L8 22l10-10L8 2L6.23 3.77L14.46 12z" fill="currentColor"></path>
+    </svg>
+  )
+}
