53e69ebfb91974001bd94920940b185bb36ef072
kreako <olivier@kreako.fr>
2022-04-24 23:01:58 +0200

    Update favicon

--- next/public/convert.sh
+++ next/public/convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-inkscape favicon.svg --export-filename=favicon.png
-convert favicon.png -resize 32x32 favicon.ico
+# inkscape favicon.svg --export-filename=favicon.png
+# convert favicon.png -resize 32x32 favicon.ico
 # convert avatar_src.png -resize 64x64 avatar.png
+
+convert favicon.jpg -resize 32x32 favicon.ico 
